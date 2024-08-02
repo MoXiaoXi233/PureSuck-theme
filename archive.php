38e67ae15d0131449a0037752045ccf1bdb5b7e4
MoXiify <3188468169@qq.com>
2024-08-02 22:56:33 +0800

    img_fix

--- archive.php
+++ archive.php
@@ -23,7 +23,7 @@
 
                 <?php if ($hasImg): ?>
                 <figure class="post-media <?= $this->is('post') ? 'single' : ''; ?>">
-                    <img itemprop="image" src="<?php $this->fields->img(); ?>" alt="头图">
+                    <img itemprop="image" src="<?php $this->fields->img(); ?>" alt="头图" loading="lazy" width="2000" height="800">
                 </figure>
                 <?php endif; ?>
 
