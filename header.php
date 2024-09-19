ccc4742c402aa7e46f4066d5873a9484c736479f
MoXiify <3188468169@qq.com>
2024-09-20 00:18:21 +0800

    尝试修复一些问题

--- header.php
+++ header.php
@@ -10,15 +10,14 @@
     <meta name="description" content="<?= empty($this->fields->description) || !$this->is('single') ? ($this->getDescription() ?: '') : $this->fields->description; ?>" />
     <title>
         <?php $this->archiveTitle([
-            'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
+            'category' => _t('分类 %s 下的文章'),
+            'search' => _t('包含关键字 %s 的文章'),
+            'tag' => _t('标签 %s 下的文章'),
+            'author' => _t('%s 发布的文章')
         ], '', ' - '); ?>
         <?= $this->options->title(); ?>
     </title>
     <?php generateDynamicCSS(); ?>
-    
     <!-- Initial Theme Script -->
     <script>
         (function() {
@@ -28,7 +27,6 @@
             document.documentElement.setAttribute('data-theme', initialTheme);
         })();
     </script>
-    
     <!-- Dark Mode -->
     <script>
         function setTheme(theme) {
@@ -45,12 +43,14 @@
 
         function updateIcon(theme) {
             const iconElement = document.getElementById('theme-icon');
-            if (theme === 'light') {
-                iconElement.classList.remove('icon-moon-inv');
-                iconElement.classList.add('icon-sun-inv');
-            } else {
-                iconElement.classList.remove('icon-sun-inv');
-                iconElement.classList.add('icon-moon-inv');
+            if (iconElement) {
+                if (theme === 'light') {
+                    iconElement.classList.remove('icon-moon-inv');
+                    iconElement.classList.add('icon-sun-inv');
+                } else {
+                    iconElement.classList.remove('icon-sun-inv');
+                    iconElement.classList.add('icon-moon-inv');
+                }
             }
         }
 
@@ -61,25 +61,20 @@
             setTheme(initialTheme);
         });
     </script>
-    
     <!-- Style CSS -->
     <link rel="stylesheet" href="<?= $this->options->themeUrl('css/PureSuck_Style.css'); ?>">
-
     <!-- AOS -->
     <script src="<?php $this->options->themeUrl('/js/aos.js'); ?>"></script>
-
     <!-- ICON Setting -->
     <link rel="icon" href="<?= isset($this->options->logoUrl) && $this->options->logoUrl ? $this->options->logoUrl : $this->options->themeUrl . '/images/avatar.ico'; ?>" type="image/x-icon">
     <!-- CSS引入 -->
-    <link href="<?php $this->options->themeUrl('/css/a11y-dark.min.css'); ?>" rel="stylesheet"">
-    <link href=" <?php echo $this->options->themeUrl('css/PureSuck_Module.css'); ?>" rel="stylesheet">
+    <link href="<?php $this->options->themeUrl('/css/a11y-dark.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo $this->options->themeUrl('css/PureSuck_Module.css'); ?>" rel="stylesheet">
     <!-- JS引入 -->
     <script defer src="<?php $this->options->themeUrl('/js/medium-zoom.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/OwO.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/highlight.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/PureSuck_Module.js'); ?>"></script>
-
-
 </head>
 
 <body>
@@ -89,25 +84,20 @@
                 <span class="el-avatar el-avatar--circle">
                     <img src="<?= $this->options->logoIndex; ?>" style="object-fit:cover;" alt="博主头像" width="512" height="512">
                 </span>
-
                 <div class="header-title">
                     <?= $this->options->titleIndex(); ?>
                 </div>
-
                 <p itemprop="description" class="header-item header-about">
                     <?= $this->options->customDescription ?: 'ワクワク'; ?>
                 </p>
-
                 <div class="left-side-custom-code">
                     <?= $this->options->leftSideCustomCode ?: ''; ?>
                 </div>
-
                 <div class="nav header-item header-credit">
                     Powered by Typecho
                     <br>
                     <a href="https://github.com/MoXiaoXi233/PureSuck-theme">Theme PureSuck</a>
                 </div>
-
                 <nav class="nav header-item header-nav">
                     <span class="nav-item<?= $this->is('index') ? ' nav-item-current' : ''; ?>">
                         <a href="<?= $this->options->siteUrl(); ?>" title="首页">
@@ -125,7 +115,6 @@
                     <?php endwhile; ?>
                     <!--结束显示页面-->
                 </nav>
-
                 <div class="theme-toggle-container">
                     <button class="theme-toggle" onclick="toggleTheme()" aria-label="日夜切换">
                         <span id="theme-icon"></span>
@@ -133,5 +122,4 @@
                 </div>
             </div>
         </header>
-
         <main class="main">
