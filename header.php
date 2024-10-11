6b16b3f68c227de201a3ed140eac67cb48775dc9
MoXiify <3188468169@qq.com>
2024-10-11 23:15:49 +0800

    header内置接口添加

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="<?= empty($this->fields->description) || !$this->is('single') ? ($this->getDescription() ?: '') : $this->fields->description; ?>" />
+    <?php $this->header(); ?>
     <title>
         <?php $this->archiveTitle([
             'category' => _t('分类 %s 下的文章'),
