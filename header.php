7f7951b6b13e2df7e359a83b618aa87430c7edbf
MoXiify <3188468169@qq.com>
2024-10-19 23:30:48 +0800

    修个规范问题和Windows窗字体颜色问题

--- header.php
+++ header.php
@@ -99,7 +99,7 @@
                 mask-size: <?php echo '40px 12px'; ?>;
                 <?php elseif ($this->options->postTitleAfter == 'handDrawn'): ?>
                 /* 添加手绘风格的样式 */
-                /* 这里可以添加具体的手绘风格的样式 */
+                /* 这里可以添加具体的手绘风格的样式，不过浪费了两个小时也没写好，放弃了 */
                 <?php endif; ?>
             }
         </style>
@@ -110,7 +110,7 @@
     <link rel="icon" href="<?= isset($this->options->logoUrl) && $this->options->logoUrl ? $this->options->logoUrl : $this->options->themeUrl . '/images/avatar.ico'; ?>" type="image/x-icon">
     <!-- CSS引入 -->
     <link href="<?php $this->options->themeUrl('/css/a11y-dark.min.css'); ?>" rel="stylesheet">
-    <link href="<?php echo $this->options->themeUrl('css/PureSuck_Module.css'); ?>" rel="stylesheet">
+    <link href="<?php $this->options->themeUrl('/css/PureSuck_Module.css'); ?>" rel="stylesheet">
     <!-- JS引入 -->
     <script defer src="<?php $this->options->themeUrl('/js/medium-zoom.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/OwO.min.js'); ?>"></script>
