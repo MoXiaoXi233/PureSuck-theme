e00a0264a9fe269ff8c1b0bb94b246625483600e
MoXiify <3188468169@qq.com>
2024-11-22 00:34:54 +0800

    TypechoComment 评论区重载

--- comments.php
+++ comments.php
@@ -1,7 +1,8 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
 <?php if ($this->allow('comment')): ?>
-
+    <?php $this->header('commentReply=1&description=0&keywords=0&generator=0&template=0&pingback=0&xmlrpc=0&wlw=0&rss2=0&rss1=0&antiSpam=0&atom'); ?>
+    
     <?php
     function threadedComments($comments, $options)
     {
