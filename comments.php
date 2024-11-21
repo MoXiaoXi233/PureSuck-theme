d85e2788cf8800135007af2fc5fea11d1c095fd3
MoXiify <3188468169@qq.com>
2024-11-22 00:10:33 +0800

    重写评论区逻辑，优化 JS，修复多余代码

--- comments.php
+++ comments.php
@@ -1,30 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
 <?php if ($this->allow('comment')): ?>
-    <script>
-        function tg_c(id, nc) {
-            var e = document.getElementById(id);
-            var c = e.className;
-            if (!c) {
-                e.className = nc;
-            } else {
-                var classArr = c.split(' ');
-                var exist = false;
-                for (var i = 0; i < classArr.length; i++) {
-                    if (classArr[i] === nc) {
-                        classArr.splice(i, 1);
-                        e.className = Array.prototype.join.call(classArr, " ");
-                        exist = true;
-                        break;
-                    }
-                }
-                if (!exist) {
-                    classArr.push(nc);
-                    e.className = Array.prototype.join.call(classArr, " ");
-                }
-            }
-        }
-    </script>
 
     <?php
     function threadedComments($comments, $options)
@@ -74,80 +50,54 @@
             <div class="ccr">
                 <?php $comments->cancelReply(); ?>
             </div>
-            <div id="<?php $this->respondId(); ?>" class="respond">
-                <div class="ccr">
-                    <?php $comments->cancelReply(); ?>
-                </div>
-                <div class="response comment-title icon-chat">发表新评论</div>
-                <form method="post" action="<?php $this->commentUrl() ?>" id="cf">
-                    <?php if ($this->user->hasLogin()): ?>
-                        <span>亲爱的<a href="<?php $this->options->profileUrl(); ?>">
-                                <?php $this->user->screenName(); ?>
-                            </a>~ <a href="<?php $this->options->logoutUrl(); ?>" title="Logout">退出 &raquo;</a>
+            <div class="response comment-title icon-chat">发表新评论</div>
+            <form method="post" action="<?php $this->commentUrl() ?>" id="cf">
+                <?php if ($this->user->hasLogin()): ?>
+                    <span>亲爱的<a href="<?php $this->options->profileUrl(); ?>">
+                            <?php $this->user->screenName(); ?>
+                        </a>~ <a href="<?php $this->options->logoutUrl(); ?>" title="Logout">退出 &raquo;</a>
+                    </span>
+                <?php else: ?>
+                    <?php if ($this->remember('author', true) != "" && $this->remember('mail', true) != ""): ?>
+                        <span>欢迎
+                            <?php $this->remember('author'); ?> 回来~
                         </span>
-                    <?php else: ?>
-                        <?php if ($this->remember('author', true) != "" && $this->remember('mail', true) != ""): ?>
-                            <span>欢迎
-                                <?php $this->remember('author'); ?> 回来~
-                            </span>
-                        <?php endif; ?>
-                        <div class="ainfo">
-                            <div class="tbox-container">
-                                <div class="tbox">
-                                    <input type="text" name="author" id="author" class="ci" placeholder="您的昵称" value="<?php $this->remember('author'); ?>" required="">
-                                </div>
-                                <div class="tbox">
-                                    <input type="email" name="mail" id="mail" class="ci" placeholder="邮箱地址" value="<?php $this->remember('mail'); ?>" required="">
-                                </div>
-                                <div class="tbox">
-                                    <input type="url" name="url" id="url" class="ci" placeholder="您的网站（选填）" value="<?php $this->remember('url'); ?>">
-                                </div>
-                            </div>
-                        </div>
                     <?php endif; ?>
-                    <div class="tbox">
-                        <textarea name="text" id="textarea" class="ci OwO-textarea" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};" placeholder="请在这里输入您的评论内容" required><?php $this->remember('text', false); ?></textarea>
-                        <div class="CtBoxBar">
-                            <div class="left-bar">
-                                <div class="OwO-bar">
-                                    <div class="OwO"></div>
-                                </div>
-                                <!-- 未来可以在这里添加更多内容 -->
+                    <div class="ainfo">
+                        <div class="tbox-container">
+                            <div class="tbox">
+                                <input type="text" name="author" id="author" class="ci" placeholder="您的昵称" value="<?php $this->remember('author'); ?>" required="">
                             </div>
-                            <button type="submit" class="submit" id="submit">提交评论</button>
+                            <div class="tbox">
+                                <input type="email" name="mail" id="mail" class="ci" placeholder="邮箱地址" value="<?php $this->remember('mail'); ?>" required="">
+                            </div>
+                            <div class="tbox">
+                                <input type="url" name="url" id="url" class="ci" placeholder="您的网站（选填）" value="<?php $this->remember('url'); ?>">
+                            </div>
                         </div>
                     </div>
-                </form>
-            </div>
+                <?php endif; ?>
+                <div class="tbox">
+                    <textarea name="text" id="textarea" class="ci OwO-textarea" onkeydown="if(event.ctrlKey&&event.keyCode==13){document.getElementById('submit').click();return false};" placeholder="请在这里输入您的评论内容" required><?php $this->remember('text', false); ?></textarea>
+                    <div class="CtBoxBar">
+                        <div class="left-bar">
+                            <div class="OwO-bar">
+                                <div class="OwO"></div>
+                            </div>
+                            <!-- 未来可以在这里添加更多内容 -->
+                        </div>
+                        <button type="submit" class="submit" id="submit">提交评论</button>
+                    </div>
+                </div>
+            </form>
         </div>
+    </div>
 
-        <!-- CSS引入 -->
-        <link rel="stylesheet" href="<?= $this->options->themeUrl('css/OwO.min.css'); ?>">
-        <script src="<?php $this->options->themeUrl('/js/OwO.min.js'); ?>"></script>
-        <script>
-            function initializeCommentsOwO() {
-                var OwO_demo = new OwO({
-                    logo: 'OωO表情',
-                    container: document.getElementsByClassName('OwO')[0],
-                    target: document.getElementsByClassName('OwO-textarea')[0],
-                    api: '/usr/themes/PureSuck/js/OwO.json',
-                    position: 'up',
-                    width: '100%',
-                    maxHeight: '260px'
-                });
+    <!-- CSS引入 -->
+    <link rel="stylesheet" href="<?= $this->options->themeUrl('css/OwO.min.css'); ?>">
 
-                var owoButton = document.getElementById('owo-button');
-                if (owoButton) {
-                    owoButton.addEventListener('click', function(event) {
-                        OwO_demo.toggle();
-                    });
-                }
-            }
-
-            initializeCommentsOwO()
-        </script>
-    <?php else: ?>
+<?php else: ?>
+    <div>
+        <h3><?php _e('评论已关闭'); ?></h3>
         <div>
-            <h3><?php _e('评论已关闭'); ?></h3>
-            <div>
-            <?php endif; ?>
+        <?php endif; ?>
